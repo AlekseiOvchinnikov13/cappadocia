fdf6876405460e180bd718869e248f5d63bd0689
Aleksei <eastonsc6@yandex.ru>
2020-08-18 13:32:18 +0300

    сверстал контент

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,11 @@ $(function(){
     /*$('.header-arrows').on('click', function() {
         $('html,body').animate({scrollTop:$('.surf').offset().top+"px"},{duration:1E3});
     });*/
-    $('.fairy-tail-slider').slick({
-        // prevArrow:
-        // nextArrow:
+    $('.fairy-tail-slider,.our-trip-slider').slick({
+        prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-next.svg" alt=""></button>',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true
     });
 });
